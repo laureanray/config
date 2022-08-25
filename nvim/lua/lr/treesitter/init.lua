fa11dfb3f9ffb19d9fe4d05c992d5512533357c3
laureanray <laureanraybahala@gmail.com>
2022-08-25 19:35:53 +0800

    removed inline virtual text

--- nvim/lua/lr/treesitter/init.lua
+++ nvim/lua/lr/treesitter/init.lua
@@ -26,14 +26,7 @@ npairs.add_rules({
 vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics,
   {
-      virtual_text = {
-        underline = true,
-        virtual_text = {
-            spacing = 5,
-            severity_limit = 'Warning',
-        },
-        update_in_insert = true,
-    }
+      virtual_text = false
   }
 )
 
