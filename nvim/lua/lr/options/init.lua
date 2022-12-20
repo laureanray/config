63dbb9180878a8502965dcb463e06aa1d91fff35
laureanray <laureanraybahala@gmail.com>
2022-12-20 21:32:35 +0800

    fix inccomand

--- nvim/lua/lr/options/init.lua
+++ nvim/lua/lr/options/init.lua
@@ -1,11 +1,11 @@
-vim.cmd('filetype plugin indent on')
-vim.o.shortmess = vim.o.shortmess .. 'c'
+vim.cmd("filetype plugin indent on")
+vim.o.shortmess = vim.o.shortmess .. "c"
 vim.o.hidden = true
-vim.o.whichwrap = 'b,s,<,>,[,],h,l'
+vim.o.whichwrap = "b,s,<,>,[,],h,l"
 vim.o.pumheight = 10
-vim.o.fileencoding = 'utf-8'
+vim.o.fileencoding = "utf-8"
 --vim.o.splitbelow = true
-vim.o.splitright = true
+-- vim.o.splitright = true
 vim.opt.termguicolors = true
 vim.o.conceallevel = 0
 vim.o.showtabline = 2
@@ -19,7 +19,9 @@ vim.o.scrolloff = 3
 vim.o.sidescrolloff = 5
 vim.o.mouse = "a"
 vim.wo.wrap = false
-vim.wo.number = true vim.wo.relativenumber = true vim.o.cursorline = true
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.o.cursorline = true
 vim.wo.signcolumn = "yes"
 vim.o.tabstop = 2
 vim.bo.tabstop = 2
@@ -31,31 +33,31 @@ vim.bo.autoindent = true
 vim.o.expandtab = true
 vim.bo.expandtab = true
 vim.o.ignorecase = true
-vim.o.spelllang = en,cjk
-vim.o.spellsuggest=best,9
+vim.o.spelllang = en, cjk
+vim.o.spellsuggest = best, 9
 vim.o.updatetime = 300
-vim.wo.signcolumn = 'yes'
+vim.wo.signcolumn = "yes"
 
 -- undo tree
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true 
+vim.opt.undofile = true
 
-vim.cmd[[
+vim.cmd([[
+set inccommand=nosplit
 set mmp=5000
-]]
+]])
 -- Auto format
-vim.g.neoformat_enabled_typescript = {"eslint_d"}
-vim.g.neoformat_enabled_typescriptreact = {"eslint_d"}
-vim.g.neoformat_enabled_go = {"gofmt"}
+vim.g.neoformat_enabled_typescript = { "eslint_d" }
+vim.g.neoformat_enabled_typescriptreact = { "eslint_d" }
+vim.g.neoformat_enabled_go = { "gofmt" }
 --vim.g.neoformat_enabled_prisma = {"prisma format"}
-vim.cmd[[
+vim.cmd([[
         imap <silent><script><expr> <C-J> copilot#Accept("\<CR>")
         let g:copilot_no_tab_map = v:true
-]]
+]])
 
-vim.o.inccommand = nosplit
 local opt = vim.opt
 
 opt.foldcolumn = "1"
@@ -63,11 +65,10 @@ opt.foldlevel = 99
 opt.foldlevelstart = -1
 opt.foldenable = true
 
-
-vim.cmd[[
+vim.cmd([[
   call github_colors#togglebg_map('<f5>')
-]]
-  
+]])
+
 -- general
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
@@ -75,6 +76,7 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 vim.opt.colorcolumn = "80"
 
-
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
+
+vim.o.inccommand = "nosplit"
--- nvim/lua/lr/remap/init.lua
+++ nvim/lua/lr/remap/init.lua
@@ -1,3 +1,5 @@
+vim.g.mapleader = " "
+
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
@@ -5,4 +7,15 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 -- Greatest REMAP ever, sabi ni Prime
-vim.keymap.set("x", "<leader>p", "\"_dp")
+vim.keymap.set("x", "<leader>p", '"_dp')
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
