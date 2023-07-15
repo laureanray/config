5e029dad916cb904f90e41bddd055d291bf5590a
laureanray <laureanraybahala@gmail.com>
2023-07-15 21:28:06 +0800

    fish stuff

---
+++ fish/config.fish
@@ -0,0 +1,15 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+set --export PATH /home/lr/.cargo/bin $PATH
+
+# pnpm
+set -gx PNPM_HOME "/home/lr/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,1 @@
+/home/lr/.local/share/omf/themes/boxfish/fish_prompt.fish
