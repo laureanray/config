4f79719b2688cddda97abe5ce3785deb464a3d99
laureanray <laureanraybahala@gmail.com>
2023-07-19 13:31:44 +0800

    fix formatting

--- nvim/lua/lr/color-scheme/init.lua
+++ nvim/lua/lr/color-scheme/init.lua
@@ -1,153 +1,111 @@
 if vim.fn.has("mac") == 1 then
-local dracula = require("dracula")
-dracula.setup({
-  -- customize dracula color palette
-  colors = {
-    bg = "#282A36",
-    fg = "#F8F8F2",
-    selection = "#44475A",
-    comment = "#6272A4",
-    red = "#FF5555",
-    orange = "#FFB86C",
-    yellow = "#F1FA8C",
-    green = "#50fa7b",
-    purple = "#BD93F9",
-    cyan = "#8BE9FD",
-    pink = "#FF79C6",
-    bright_red = "#FF6E6E",
-    bright_green = "#69FF94",
-    bright_yellow = "#FFFFA5",
-    bright_blue = "#D6ACFF",
-    bright_magenta = "#FF92DF",
-    bright_cyan = "#A4FFFF",
-    bright_white = "#FFFFFF",
-    menu = "#21222C",
-    visual = "#3E4452",
-    gutter_fg = "#4B5263",
-    nontext = "#3B4048",
-    white = "#ABB2BF",
-    black = "#191A21",
-  },
-  -- show the '~' characters after the end of buffers
-  show_end_of_buffer = true, -- default false
-  -- use transparent background
-  transparent_bg = true, -- default false
-  -- set custom lualine background color
-  lualine_bg_color = "#44475a", -- default nil
-  -- set italic comment
-  italic_comment = true, -- default false
-  -- overrides the default highlights with table see `:h synIDattr`
-  overrides = {},
-  -- You can use overrides as table like this
-  -- overrides = {
-  --   NonText = { fg = "white" }, -- set NonText fg to white
-  --   NvimTreeIndentMarker = { link = "NonText" }, -- link to NonText highlight
-  --   Nothing = {} -- clear highlight of Nothing
-  -- },
-  -- Or you can also use it like a function to get color from theme
-  -- overrides = function (colors)
-  --   return {
-  --     NonText = { fg = colors.white }, -- set NonText fg to white of theme
-  --   }
-  -- end,
-})
+  local dracula = require("dracula")
+  dracula.setup({
+    -- customize dracula color palette
+    colors = {
+      bg = "#282A36",
+      fg = "#F8F8F2",
+      selection = "#44475A",
+      comment = "#6272A4",
+      red = "#FF5555",
+      orange = "#FFB86C",
+      yellow = "#F1FA8C",
+      green = "#50fa7b",
+      purple = "#BD93F9",
+      cyan = "#8BE9FD",
+      pink = "#FF79C6",
+      bright_red = "#FF6E6E",
+      bright_green = "#69FF94",
+      bright_yellow = "#FFFFA5",
+      bright_blue = "#D6ACFF",
+      bright_magenta = "#FF92DF",
+      bright_cyan = "#A4FFFF",
+      bright_white = "#FFFFFF",
+      menu = "#21222C",
+      visual = "#3E4452",
+      gutter_fg = "#4B5263",
+      nontext = "#3B4048",
+      white = "#ABB2BF",
+      black = "#191A21",
+    },
+    -- show the '~' characters after the end of buffers
+    show_end_of_buffer = true,  -- default false
+    -- use transparent background
+    transparent_bg = true,      -- default false
+    -- set custom lualine background color
+    lualine_bg_color = "#44475a", -- default nil
+    -- set italic comment
+    italic_comment = true,      -- default false
+    -- overrides the default highlights with table see `:h synIDattr`
+    overrides = {},
+    -- You can use overrides as table like this
+    -- overrides = {
+    --   NonText = { fg = "white" }, -- set NonText fg to white
+    --   NvimTreeIndentMarker = { link = "NonText" }, -- link to NonText highlight
+    --   Nothing = {} -- clear highlight of Nothing
+    -- },
+    -- Or you can also use it like a function to get color from theme
+    -- overrides = function (colors)
+    --   return {
+    --     NonText = { fg = colors.white }, -- set NonText fg to white of theme
+    --   }
+    -- end,
+  })
   vim.cmd('colorscheme dracula')
 elseif vim.fn.has("unix") == 1 then
-local dracula = require("dracula")
-dracula.setup({
-  -- customize dracula color palette
-  colors = {
-    bg = "#282A36",
-    fg = "#F8F8F2",
-    selection = "#44475A",
-    comment = "#6272A4",
-    red = "#FF5555",
-    orange = "#FFB86C",
-    yellow = "#F1FA8C",
-    green = "#50fa7b",
-    purple = "#BD93F9",
-    cyan = "#8BE9FD",
-    pink = "#FF79C6",
-    bright_red = "#FF6E6E",
-    bright_green = "#69FF94",
-    bright_yellow = "#FFFFA5",
-    bright_blue = "#D6ACFF",
-    bright_magenta = "#FF92DF",
-    bright_cyan = "#A4FFFF",
-    bright_white = "#FFFFFF",
-    menu = "#21222C",
-    visual = "#3E4452",
-    gutter_fg = "#4B5263",
-    nontext = "#3B4048",
-    white = "#ABB2BF",
-    black = "#191A21",
-  },
-  -- show the '~' characters after the end of buffers
-  show_end_of_buffer = true, -- default false
-  -- use transparent background
-  transparent_bg = true, -- default false
-  -- set custom lualine background color
-  lualine_bg_color = "#44475a", -- default nil
-  -- set italic comment
-  italic_comment = true, -- default false
-  -- overrides the default highlights with table see `:h synIDattr`
-  overrides = {},
-  -- You can use overrides as table like this
-  -- overrides = {
-  --   NonText = { fg = "white" }, -- set NonText fg to white
-  --   NvimTreeIndentMarker = { link = "NonText" }, -- link to NonText highlight
-  --   Nothing = {} -- clear highlight of Nothing
-  -- },
-  -- Or you can also use it like a function to get color from theme
-  -- overrides = function (colors)
-  --   return {
-  --     NonText = { fg = colors.white }, -- set NonText fg to white of theme
-  --   }
-  -- end,
-})
+  local dracula = require("dracula")
+  dracula.setup({
+    -- customize dracula color palette
+    colors = {
+      bg = "#282A36",
+      fg = "#F8F8F2",
+      selection = "#44475A",
+      comment = "#6272A4",
+      red = "#FF5555",
+      orange = "#FFB86C",
+      yellow = "#F1FA8C",
+      green = "#50fa7b",
+      purple = "#BD93F9",
+      cyan = "#8BE9FD",
+      pink = "#FF79C6",
+      bright_red = "#FF6E6E",
+      bright_green = "#69FF94",
+      bright_yellow = "#FFFFA5",
+      bright_blue = "#D6ACFF",
+      bright_magenta = "#FF92DF",
+      bright_cyan = "#A4FFFF",
+      bright_white = "#FFFFFF",
+      menu = "#21222C",
+      visual = "#3E4452",
+      gutter_fg = "#4B5263",
+      nontext = "#3B4048",
+      white = "#ABB2BF",
+      black = "#191A21",
+    },
+    -- show the '~' characters after the end of buffers
+    show_end_of_buffer = true,  -- default false
+    -- use transparent background
+    transparent_bg = true,      -- default false
+    -- set custom lualine background color
+    lualine_bg_color = "#44475a", -- default nil
+    -- set italic comment
+    italic_comment = true,      -- default false
+    -- overrides the default highlights with table see `:h synIDattr`
+    overrides = {},
+    -- You can use overrides as table like this
+    -- overrides = {
+    --   NonText = { fg = "white" }, -- set NonText fg to white
+    --   NvimTreeIndentMarker = { link = "NonText" }, -- link to NonText highlight
+    --   Nothing = {} -- clear highlight of Nothing
+    -- },
+    -- Or you can also use it like a function to get color from theme
+    -- overrides = function (colors)
+    --   return {
+    --     NonText = { fg = colors.white }, -- set NonText fg to white of theme
+    --   }
+    -- end,
+  })
   vim.cmd('colorscheme dracula')
 else
-	print("Unsupported system for sumneko")
+  print("Unsupported system for sumneko")
 end
-
--- Customization for Pmenu
--- vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#282C34", fg = "NONE" })
--- vim.api.nvim_set_hl(0, "Pmenu", { fg = "#C5CDD9", bg = "#22252A" })
--- 
--- vim.api.nvim_set_hl(0, "CmpItemAbbrDeprecated", { fg = "#7E8294", bg = "NONE", strikethrough = true })
--- vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { fg = "#82AAFF", bg = "NONE", bold = true })
--- vim.api.nvim_set_hl(0, "CmpItemAbbrMatchFuzzy", { fg = "#82AAFF", bg = "NONE", bold = true })
--- vim.api.nvim_set_hl(0, "CmpItemMenu", { fg = "#C792EA", bg = "NONE", italic = true })
--- 
--- vim.api.nvim_set_hl(0, "CmpItemKindField", { fg = "#EED8DA", bg = "#B5585F" })
--- vim.api.nvim_set_hl(0, "CmpItemKindProperty", { fg = "#EED8DA", bg = "#B5585F" })
--- vim.api.nvim_set_hl(0, "CmpItemKindEvent", { fg = "#EED8DA", bg = "#B5585F" })
--- 
--- vim.api.nvim_set_hl(0, "CmpItemKindText", { fg = "#C3E88D", bg = "#9FBD73" })
--- vim.api.nvim_set_hl(0, "CmpItemKindEnum", { fg = "#C3E88D", bg = "#9FBD73" })
--- vim.api.nvim_set_hl(0, "CmpItemKindKeyword", { fg = "#C3E88D", bg = "#9FBD73" })
--- 
--- vim.api.nvim_set_hl(0, "CmpItemKindConstant", { fg = "#FFE082", bg = "#D4BB6C" })
--- vim.api.nvim_set_hl(0, "CmpItemKindConstructor", { fg = "#FFE082", bg = "#D4BB6C" })
--- vim.api.nvim_set_hl(0, "CmpItemKindReference", { fg = "#FFE082", bg = "#D4BB6C" })
--- 
--- vim.api.nvim_set_hl(0, "CmpItemKindFunction", { fg = "#EADFF0", bg = "#A377BF" })
--- vim.api.nvim_set_hl(0, "CmpItemKindStruct", { fg = "#EADFF0", bg = "#A377BF" })
--- vim.api.nvim_set_hl(0, "CmpItemKindClass", { fg = "#EADFF0", bg = "#A377BF" })
--- vim.api.nvim_set_hl(0, "CmpItemKindModule", { fg = "#EADFF0", bg = "#A377BF" })
--- vim.api.nvim_set_hl(0, "CmpItemKindOperator", { fg = "#EADFF0", bg = "#A377BF" })
--- 
--- vim.api.nvim_set_hl(0, "CmpItemKindVariable", { fg = "#C5CDD9", bg = "#7E8294" })
--- vim.api.nvim_set_hl(0, "CmpItemKindFile", { fg = "#C5CDD9", bg = "#7E8294" })
--- 
--- vim.api.nvim_set_hl(0, "CmpItemKindUnit", { fg = "#F5EBD9", bg = "#D4A959" })
--- vim.api.nvim_set_hl(0, "CmpItemKindSnippet", { fg = "#F5EBD9", bg = "#D4A959" })
--- vim.api.nvim_set_hl(0, "CmpItemKindFolder", { fg = "#F5EBD9", bg = "#D4A959" })
--- 
--- vim.api.nvim_set_hl(0, "CmpItemKindMethod", { fg = "#DDE5F5", bg = "#6C8ED4" })
--- vim.api.nvim_set_hl(0, "CmpItemKindValue", { fg = "#DDE5F5", bg = "#6C8ED4" })
--- vim.api.nvim_set_hl(0, "CmpItemKindEnumMember", { fg = "#DDE5F5", bg = "#6C8ED4" })
--- 
--- vim.api.nvim_set_hl(0, "CmpItemKindInterface", { fg = "#D8EEEB", bg = "#58B5A8" })
--- vim.api.nvim_set_hl(0, "CmpItemKindColor", { fg = "#D8EEEB", bg = "#58B5A8" })
--- vim.api.nvim_set_hl(0, "CmpItemKindTypeParameter", { fg = "#D8EEEB", bg = "#58B5A8" })
