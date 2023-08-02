102a2c19f81d0849c1a4af4424422fa2286171d3
laureanray <laureanraybahala@gmail.com>
2023-08-03 01:45:19 +0800

    ye

--- nvim/lua/lr/options/init.lua
+++ nvim/lua/lr/options/init.lua
@@ -4,7 +4,7 @@ vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
-vim.opt.colorcolumn = "80"
+-- vim.opt.colorcolumn = "200"
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 vim.o.inccommand = "nosplit"
