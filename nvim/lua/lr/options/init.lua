7ecbe225f1cb85093a1fa9d5c5730363efe00a73
laureanray <laureanraybahala@gmail.com>
2023-08-04 19:25:55 +0800

    add goyo conf

--- nvim/lua/lr/options/init.lua
+++ nvim/lua/lr/options/init.lua
@@ -57,3 +57,6 @@ opt.foldlevelstart = -1
 opt.foldenable = true
 
 vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+
+vim.g.goyo_height = "95%"
+vim.g.goyo_width = 105
