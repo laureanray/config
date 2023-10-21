7d3b7d52fe688459a2981cd9001517b3c33ee744
laureanray <laureanraybahala@gmail.com>
2023-10-21 15:34:31 +0800

    sa

--- nvim/lua/lr/listchars/init.lua
+++ nvim/lua/lr/listchars/init.lua
@@ -9,7 +9,7 @@ vim.opt.termguicolors = true
 vim.opt.list = true
 vim.opt.listchars:append "eol:↲"
 vim.opt.listchars:append "space:⋅"
-vim.opt.listchars:append "tab:▸ "
+vim.opt.listchars:append "tab:│ "
 
 local highlight = {
     "RainbowRed",
