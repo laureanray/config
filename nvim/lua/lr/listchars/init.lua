a59a8c81aac95fa950ac66d1fe2fd86daaa071b8
laureanray <laureanraybahala@gmail.com>
2023-10-24 00:03:31 +0800

    fix eol

--- nvim/lua/lr/listchars/init.lua
+++ nvim/lua/lr/listchars/init.lua
@@ -7,9 +7,10 @@ vim.opt.termguicolors = true
 -- vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
 
 vim.opt.list = true
-vim.opt.listchars:append "eol:↲"
-vim.opt.listchars:append "space:⋅"
-vim.opt.listchars:append "tab:│ "
+-- vim.opt.listchars:append "eol:↲"
+-- vim.opt.listchars:append "space:⋅"
+-- vim.opt.listchars:append "tab:│ "
+vim.opt.listchars = {tab = '⁞ ', eol = '¬', trail = '·', space = '⋅', extends = '»', precedes = '«'}
 
 local highlight = {
     "RainbowRed",
